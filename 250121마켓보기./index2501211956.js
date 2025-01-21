c839bb673bde0c49e10f79db60cc87a30ad880a2
Jung <106378087+dlvvkfl88@users.noreply.github.com>
2025-01-21 19:56:23 +0900

    Update and rename index2501211838.js to index2501211956.js

--- 250121마켓보기./index2501211956.js
+++ 250121마켓보기./index2501211956.js
@@ -5139,8 +5139,7 @@ app.get('/reusable_market_new', session_exists, async function (req, res) {
 	}
 
 	// 기본 쿼리문
-	let baseQuery = 'SELECT a.*,b.sImgPath,b.sImgBin FROM tblReusable a, tblReusableImg b WHERE a.nReusableNo=b.nReusableIdx';
-
+	let baseQuery = 'SELECT a.*,b.sImgPath,b.sImgBin FROM tblReusable a, tblReusableImg b WHERE a.nReusableNo=b.nReusableIdx AND (a.nApplicantState < 2 OR a.nReusableState < 2)';
 	// 검색 조건 추가
 	if (searchKeyword && searchType !== '0') {
 		switch (searchType) {
@@ -14056,245 +14055,214 @@ app.post('/confirmReceipt', async function (req, res) {
 // });
 // 위것과 구분
 app.post('/completeApplicantReceipt', async function (req, res) {
-	var resu_no = req.body.resu_no;
+	try {
+		var resu_no = req.body.resu_no;
 
-	// 신청자 상태를 수령 완료(3)로 업데이트
-	var sql = "UPDATE tblReusable SET nApplicantState=? WHERE nReusableNo=?";
-	await directQuery(sql, [3, resu_no]);
+		// 신청자 상태를 수령 완료(3)로 업데이트
+		var sql = "UPDATE tblReusable SET nApplicantState=? WHERE nReusableNo=?";
+		await directQuery(sql, [3, resu_no]);
 
-	// 재사용 물품 정보 조회
-	sql = "SELECT * FROM tblReusable WHERE nReusableNo=?";
-	var sql_res = await directQuery(sql, [resu_no]);
+		// 재사용 물품 정보 조회
+		sql = "SELECT * FROM tblReusable WHERE nReusableNo=?";
+		var sql_res = await directQuery(sql, [resu_no]);
 
-	// 둘 다 수령 완료 상태(3)일 때만 처리
-	if (sql_res[0].nReusableState == 3 && sql_res[0].nApplicantState == 3) {
-		// 재사용 물품인 경우
-		if (sql_res[0].nReusableShare == 0) {
-			var PrevUser = sql_res[0].sReusableNowUser;
+		// 둘 다 수령 완료 상태(3)일 때만 처리
+		if (sql_res[0].nReusableState == 3 && sql_res[0].nApplicantState == 3) {
+			// 재사용 물품인 경우
+			if (sql_res[0].nReusableShare == 0) {
+				var PrevUser = sql_res[0].sReusableNowUser;
+				// 신청자 정보 초기화
+				var ApplicantEmail = "";
+				var ApplicantName = sql_res[0].sReusableApplicant;
+				var ApplicantOrg = "";
+				var ApplicantUserOrg = "";
+				var ApplicantUserNo = "";
+				var ApplicantUserDptCode = "";
 
-			// 신청자 정보 초기화
-			var ApplicantEmail = "";
-			var ApplicantName = sql_res[0].sReusableApplicant;
-			var ApplicantOrg = "";
-			var ApplicantUserOrg = "";
-			var ApplicantUserNo = "";
-			var ApplicantUserDptCode = "";
+				// 테스트 계정별 정보 설정
+				if (ApplicantName == "고려대 테스트 유저") {
+					ApplicantEmail = "korea@korea.ac.kr";
+					ApplicantOrg = "본부";
+					ApplicantUserOrg = "본부";
+					ApplicantUserNo = "000000";
+					ApplicantUserDptCode = "0000";
+				}
+				else if (ApplicantName == "원격교육센터 02") {
+					ApplicantEmail = "kbw5636@kounosoft.com";
+					ApplicantOrg = "총무부";
+					ApplicantUserOrg = "총무부";
+					ApplicantUserNo = "210597";
+					ApplicantUserDptCode = "5022";
+				}
+				else if (ApplicantName == "테스트자산관리01") {
+					ApplicantEmail = "jaebeen2@kounosoft.com";
+					ApplicantOrg = "관리팀";
+					ApplicantUserOrg = "관리팀";
+					ApplicantUserNo = "597210";
+					ApplicantUserDptCode = "5846";
+				}
+				else if (ApplicantName == "테스트자산관리02") {
+					ApplicantEmail = "sizin@kounosoft.com";
+					ApplicantOrg = "관리팀";
+					ApplicantUserOrg = "관리팀";
+					ApplicantUserNo = "666308";
+					ApplicantUserDptCode = "0014";
+				}
+				else {
+					// 실제 사용자 정보 조회
+					sql = "SELECT * FROM tblUser WHERE sName=?";
+					var email_res = await directQuery(sql, [ApplicantName]);
 
-			// 테스트 계정별 정보 설정
-			if (ApplicantName == "고려대 테스트 유저") {
-				ApplicantEmail = "korea@korea.ac.kr";
-				ApplicantOrg = "본부";
-				ApplicantUserOrg = "본부";
-				ApplicantUserNo = "000000";
-				ApplicantUserDptCode = "0000";
+					ApplicantEmail = email_res[0].sEMail;
+					ApplicantUserNo = email_res[0].sNumber;
+					ApplicantUserDptCode = email_res[0].sDepartmentCode;
+
+					// 조직 정보 조회
+					sql = "SELECT * FROM tblOrganization WHERE nIndex=?";
+					var org_res = await directQuery(sql, [email_res[0].nOrgIdx]);
+
+					ApplicantOrg = org_res[0].sName;
+					ApplicantUserOrg = org_res[0].sName;
+				}
+
+				// 재사용 물품 사용자 정보 업데이트
+				sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
+				await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
+
+				// 비품 정보 업데이트
+				sql = "UPDATE tblFixture SET sOrg=?, sUserOrg=?, sUser=?, sUserNo=?, sDepartmentCode=? WHERE sFixtureNo=?";
+				await directQuery(sql, [ApplicantOrg, ApplicantUserOrg, ApplicantName, ApplicantUserNo, ApplicantUserDptCode, sql_res[0].sFixtureNo]);
 			}
-			else if (ApplicantName == "원격교육센터 02") {
-				ApplicantEmail = "kbw5636@kounosoft.com";
-				ApplicantOrg = "총무부";
-				ApplicantUserOrg = "총무부";
-				ApplicantUserNo = "210597";
-				ApplicantUserDptCode = "5022";
-			}
-			else if (ApplicantName == "테스트자산관리01") {
-				ApplicantEmail = "jaebeen2@kounosoft.com";
-				ApplicantOrg = "관리팀";
-				ApplicantUserOrg = "관리팀";
-				ApplicantUserNo = "597210";
-				ApplicantUserDptCode = "5846";
-			}
-			else if (ApplicantName == "테스트자산관리02") {
-				ApplicantEmail = "sizin@kounosoft.com";
-				ApplicantOrg = "관리팀";
-				ApplicantUserOrg = "관리팀";
-				ApplicantUserNo = "666308";
-				ApplicantUserDptCode = "0014";
-			}
-			else {
-				// 실제 사용자 정보 조회
-				sql = "SELECT * FROM tblUser WHERE sName=?";
-				var email_res = await directQuery(sql, [ApplicantName]);
+			// 나눔 물품이고 물품 상태가 수령 완료인 경우
+			// 나눔 물품인 경우
+			else if (sql_res[0].nReusableShare == 1) {
+				var PrevUser = sql_res[0].sReusableNowUser;
+				var ApplicantName = sql_res[0].sReusableApplicant;
+				var ApplicantEmail = "";
 
-				ApplicantEmail = email_res[0].sEMail;
-				ApplicantUserNo = email_res[0].sNumber;
-				ApplicantUserDptCode = email_res[0].sDepartmentCode;
+				// 테스트 계정별 이메일 설정
+				if (ApplicantName == "고려대 테스트 유저") {
+					ApplicantEmail = "korea@korea.ac.kr";
+				}
+				else if (ApplicantName == "원격교육센터 02") {
+					ApplicantEmail = "kbw5636@kounosoft.com";
+				}
+				else if (ApplicantName == "테스트자산관리01") {
+					ApplicantEmail = "jaebeen2@kounosoft.com";
+				}
+				else if (ApplicantName == "테스트자산관리02") {
+					ApplicantEmail = "sizin@kounosoft.com";
+				}
+				else {
+					// 실제 사용자 이메일 조회
+					sql = "SELECT * FROM tblUser WHERE sName=?";
+					var email_res = await directQuery(sql, [ApplicantName]);
 
-				// 조직 정보 조회
-				sql = "SELECT * FROM tblOrganization WHERE nIndex=?";
-				var org_res = await directQuery(sql, [email_res[0].nOrgIdx]);
+					ApplicantEmail = email_res[0].sEMail;
+				}
 
-				ApplicantOrg = org_res[0].sName;
-				ApplicantUserOrg = org_res[0].sName;
+				// 나눔 물품 사용자 정보 업데이트
+				sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
+				await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
 			}
 
-			// 재사용 물품 사용자 정보 업데이트
-			sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
-			await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
-
-			// 비품 정보 업데이트
-			sql = "UPDATE tblFixture SET sOrg=?, sUserOrg=?, sUser=?, sUserNo=?, sDepartmentCode=? WHERE sFixtureNo=?";
-			await directQuery(sql, [ApplicantOrg, ApplicantUserOrg, ApplicantName, ApplicantUserNo, ApplicantUserDptCode, sql_res[0].sFixtureNo]);
+			return res.json({
+				success: true,
+				message: '인수인계가 완료되었습니다.'
+			});
+		} else {
+			return res.json({
+				success: false,
+				message: '물품과 신청자의 상태가 모두 수령 완료 상태가 아닙니다.'
+			});
 		}
-		// 나눔 물품이고 물품 상태가 수령 완료인 경우
-		else if (sql_res[0].nReusableShare == 1) {
-			var PrevUser = sql_res[0].sReusableNowUser;
-			// ... 기존 나눔 물품 처리 코드 ...
-			var ApplicantEmail = "";
-			var ApplicantName = sql_res[0].sReusableApplicant;
-
-			// 테스트 계정별 이메일 설정
-			if (ApplicantName == "고려대 테스트 유저") {
-				ApplicantEmail = "korea@korea.ac.kr";
-			}
-			else if (ApplicantName == "원격교육센터 02") {
-				ApplicantEmail = "kbw5636@kounosoft.com";
-			}
-			else if (ApplicantName == "테스트자산관리01") {
-				ApplicantEmail = "jaebeen2@kounosoft.com";
-			}
-			else if (ApplicantName == "테스트자산관리02") {
-				ApplicantEmail = "sizin@kounosoft.com";
-			}
-			else {
-				// 실제 사용자 이메일 조회
-				sql = "SELECT * FROM tblUser WHERE sName=?";
-				var email_res = await directQuery(sql, [ApplicantName]);
-
-				ApplicantEmail = email_res[0].sEMail;
-			}
-
-			// 나눔 물품 사용자 정보 업데이트
-			sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
-			console.log("PrevUser", PrevUser);
-			console.log("ApplicantName", ApplicantName);
-
-			await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
-		}
-		// 완료 메시지 응답
-		const html = `<!DOCTYPE html>
-        <html><head><meta charset="UTF-8"><title>재사용 물품 등록/신청 조회</title></head>
-        <body><script>alert ('인수인계가 완료하였습니다.');window.close ();</script></body>
-        </html>`;
-		res.send(html);
+	} catch (error) {
+		console.error('Error in completeApplicantReceipt:', error);
+		return res.status(500).json({
+			success: false,
+			message: '처리 중 오류가 발생했습니다.'
+		});
 	}
 });
 
 
 
+
 app.post('/completeReusableReceipt', async function (req, res) {
-	var resu_no = req.body.resu_no;
+	try {
+		var resu_no = req.body.resu_no;
 
-	// 물품 상태를 수령 완료(3)로 업데이트
-	var sql = "UPDATE tblReusable SET nReusableState=? WHERE nReusableNo=?";
-	await directQuery(sql, [3, resu_no]);
+		// 물품 상태를 수령 완료(3)로 업데이트
+		var sql = "UPDATE tblReusable SET nReusableState=? WHERE nReusableNo=?";
+		await directQuery(sql, [3, resu_no]);
 
-	// 재사용 물품 정보 조회
-	sql = "SELECT * FROM tblReusable WHERE nReusableNo=? ";
-	var sql_res = await directQuery(sql, [resu_no]);
+		// 재사용 물품 정보 조회
+		sql = "SELECT * FROM tblReusable WHERE nReusableNo=? ";
+		var sql_res = await directQuery(sql, [resu_no]);
 
+		// 둘 다 수령 완료 상태(3)일 때만 처리
+		if (sql_res[0].nReusableState == 3 && sql_res[0].nApplicantState == 3) {
+			// 재사용 물품인 경우
+			if (sql_res[0].nReusableShare == 0) {
+				// ... existing code for reusable items ...
+				var PrevUser = sql_res[0].sReusableNowUser;
+				var ApplicantEmail = "";
+				var ApplicantName = sql_res[0].sReusableApplicant;
+				var ApplicantOrg = "";
+				var ApplicantUserOrg = "";
+				var ApplicantUserNo = "";
+				var ApplicantUserDptCode = "";
 
+				// ... existing test user checks ...
+				if (ApplicantName == "고려대 테스트 유저") {
+					// ... existing test user logic ...
+				} else {
+					sql = "SELECT * FROM tblUser WHERE sName=? ";
+					var email_res = await directQuery(sql, [ApplicantName]);
 
-	// 둘 다 수령 완료 상태(3)일 때만 처리
-	if (sql_res[0].nReusableState == 3 && sql_res[0].nApplicantState == 3) {
-		// 재사용 물품인 경우
-		if (sql_res[0].nReusableShare == 0) {
-			var PrevUser = sql_res[0].sReusableNowUser;
+					ApplicantEmail = email_res[0].sEMail;
+					ApplicantUserNo = email_res[0].sNumber;
+					ApplicantUserDptCode = email_res[0].sDepartmentCode;
 
-			//var ApplicantEmail = sql_res[0].sReusableApplicant;
-			//var ApplicantName = "";
+					sql = "SELECT * FROM tblOrganization WHERE nIndex=? ";
+					var org_res = await directQuery(sql, [email_res[0].nOrgIdx]);
 
-			var ApplicantEmail = "";
-			var ApplicantName = sql_res[0].sReusableApplicant;
-			var ApplicantOrg = "";
-			var ApplicantUserOrg = "";
-			var ApplicantUserNo = "";
-			var ApplicantUserDptCode = "";
+					ApplicantOrg = org_res[0].sName;
+					ApplicantUserOrg = org_res[0].sName;
+				}
 
-			if (ApplicantName == "고려대 테스트 유저") {
-				ApplicantEmail = "korea@korea.ac.kr";
-				ApplicantOrg = "본부";
-				ApplicantUserOrg = "본부";
-				ApplicantUserNo = "000000";
-				ApplicantUserDptCode = "0000";
+				sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
+				await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
+
+				sql = "UPDATE tblFixture SET sOrg=?, sUserOrg=?, sUser=?, sUserNo=?, sDepartmentCode=? WHERE sFixtureNo=?";
+				await directQuery(sql, [ApplicantOrg, ApplicantUserOrg, ApplicantName, ApplicantUserNo, ApplicantUserDptCode, sql_res[0].sFixtureNo]);
 			}
-			else if (ApplicantName == "원격교육센터 02") {
-				ApplicantEmail = "kbw5636@kounosoft.com";
-				ApplicantOrg = "총무부";
-				ApplicantUserOrg = "총무부";
-				ApplicantUserNo = "210597";
-				ApplicantUserDptCode = "5022";
-			}
-			else if (ApplicantName == "테스트자산관리01") {
-				ApplicantEmail = "jaebeen2@kounosoft.com";
-				ApplicantOrg = "관리팀";
-				ApplicantUserOrg = "관리팀";
-				ApplicantUserNo = "597210";
-				ApplicantUserDptCode = "5846";
-			}
-			else if (ApplicantName == "테스트자산관리02") {
-				ApplicantEmail = "sizin@kounosoft.com";
-				ApplicantOrg = "관리팀";
-				ApplicantUserOrg = "관리팀";
-				ApplicantUserNo = "666308";
-				ApplicantUserDptCode = "0014";
-			}
-			else {
-				sql = "SELECT * FROM tblUser WHERE sName=? ";
-				var email_res = await directQuery(sql, [ApplicantName]);
+			// 나눔 물품인 경우
+			else if (sql_res[0].nReusableShare == 1) {
+				// ... existing code for shared items ...
+				var PrevUser = sql_res[0].sReusableNowUser;
+				var ApplicantName = sql_res[0].sReusableApplicant;
 
-				ApplicantEmail = email_res[0].sEMail;
-				ApplicantUserNo = email_res[0].sNumber;
-				ApplicantUserDptCode = email_res[0].sDepartmentCode;
-
-				sql = "SELECT * FROM tblOrganization WHERE nIndex=? ";
-				var org_res = await directQuery(sql, [email_res[0].nOrgIdx]);
-
-				ApplicantOrg = org_res[0].sName;
-				ApplicantUserOrg = org_res[0].sName;
+				sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
+				await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
 			}
 
-			sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
-			await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
-
-			sql = "UPDATE tblFixture SET sOrg=?, sUserOrg=?, sUser=?, sUserNo=?, sDepartmentCode=? WHERE sFixtureNo=?";
-			await directQuery(sql, [ApplicantOrg, ApplicantUserOrg, ApplicantName, ApplicantUserNo, ApplicantUserDptCode, sql_res[0].sFixtureNo]);
+			return res.json({
+				success: true,
+				message: '수령이 완료되었습니다.'
+			});
+		} else {
+			return res.json({
+				success: false,
+				message: '물품과 신청자의 상태가 모두 수령 완료 상태가 아닙니다.'
+			});
 		}
-		else if (sql_res[0].nReusableShare == 1) {
-			var PrevUser = sql_res[0].sReusableNowUser;
-
-			//var ApplicantEmail = sql_res[0].sReusableApplicant;
-			//var ApplicantName = "";
-
-			var ApplicantEmail = "";
-			var ApplicantName = sql_res[0].sReusableApplicant;
-
-			if (ApplicantName == "고려대 테스트 유저") {
-				ApplicantEmail = "korea@korea.ac.kr";
-			}
-			else if (ApplicantName == "원격교육센터 02") {
-				ApplicantEmail = "kbw5636@kounosoft.com";
-			}
-			else if (ApplicantName == "테스트자산관리01") {
-				ApplicantEmail = "jaebeen2@kounosoft.com";
-			}
-			else if (ApplicantName == "테스트자산관리02") {
-				ApplicantEmail = "sizin@kounosoft.com";
-			}
-			else {
-				sql = "SELECT * FROM tblUser WHERE sName=? ";
-				var email_res = await directQuery(sql, [ApplicantName]);
-
-				ApplicantEmail = email_res[0].sEMail;
-			}
-
-			sql = "UPDATE tblReusable SET sReusablePrevUser=?, sReusableNowUser=?, dReusableDoneDate=NOW() WHERE nReusableNo=?";
-			await directQuery(sql, [PrevUser, ApplicantName, resu_no]);
-		}
-
-		const html = `<!DOCTYPE html>
-	<html><head><meta charset="UTF-8"><title>재사용 물품 등록/신청 조회</title></head>
-	<body><script>alert ('수령이 완료되었습니다.');window.close ();</script></body>
-	</html>`;
-		res.send(html);
+	} catch (error) {
+		console.error('Error in completeReusableReceipt:', error);
+		return res.status(500).json({
+			success: false,
+			message: '처리 중 오류가 발생했습니다.'
+		});
 	}
 });
 
